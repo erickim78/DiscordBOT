c1ac7da14eb55301e817d2171324d7942998e388
ericdkim78@gmail.com <ericdkim78@gmail.com>
2019-05-31 03:30:26 -0700

    Added .gitignore and json config file

--- bot.py
+++ bot.py
@@ -1,19 +1,25 @@
 import discord
 from discord.ext import commands
 
+import json
+import sys
+
 client = commands.Bot( command_prefix = '!')
 
 @client.event
 async def on_ready():
     print('Yes Bossu.')
 
-@client.event
-async def on_voice_state_update( member, before, after) :
-    if 
-    await member.send()
-
 @client.command()
 async def ping(ctx):
     await ctx.send(f'{round(client.latency*1000)} MS')
 
+
+with open( 'config.json') as config_file:
+    data = json.load( config_file )
+
+token = data['token']
 client.run(token)
+print(token)
+
+sys.exit(0)
