49ecb57a221d8fc5959f6d459138295cc717d504
ericdkim78@gmail.com <ericdkim78@gmail.com>
2019-06-02 03:27:34 -0700

    Imported mal python wrapper

--- bot.py
+++ bot.py
@@ -1,21 +1,34 @@
+#Importing Discord 
 import discord
 from discord.ext import commands
 
+#Importing OsuAPI Python Wrapper
 import asyncio
 from pyosu import OsuApi
 
-import json
+#Importing MAL Py Wrapper
+from jikanpy import Jikan
 
+import json
+import pprint
+
+
+#Config File Usage
 with open( 'config.json') as config_file:
     temp = json.load( config_file )
 
 apikey = temp['osuapikey']
 
+
+#Instantiate API
 osu = OsuApi(apikey)
+mal = Jikan()
 
 
 client = commands.Bot( command_prefix = '!')
 
+
+#Commands/Events
 @client.event
 async def on_ready():
     print('BOT is running.')
@@ -36,13 +49,19 @@ async def on_message( message ):
             stars = beatmap.difficultyrating
 
             await message.channel.send("Username: " + username + "\nRank: " + str(user.pp_rank) + "\nTop Play: " + songname + " [" + difficultyname + "]  " + str(round(stars,2)) + "*")
-            
-
-
-@client.command()
+    elif message.content.find("!mal") != -1 :
+        inputname = realmsg[5:(len(realmsg))]
+        user = mal.user( username = inputname, request = 'animelist')
+        
+        await message.channel.send("Username: " )
+    
+     
+@client.command
 async def ping(ctx):
     await ctx.send(f'{round(client.latency*1000)} MS')
 
+
+#Run BOT
 with open( 'config.json') as config_file:
     temp = json.load( config_file )
 
