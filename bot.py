850c6f3463fb933b0de6bb64fc8c72cddc589dc3
ericdkim78@gmail.com <ericdkim78@gmail.com>
2019-06-08 03:49:43 -0700

    Added more questions to personality quiz

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ async def on_message( message ):
 
     elif channelcheck and message.content.find("!qqq") != -1 :
         personalities = [  ["BOLD", 0], ["QUIRKY", 0], ["TIMID", 0], ["NAIVE", 0], ["HASTY", 0] ]
-        questions = [1,2,3,4,5]
+        questions = [0,1,2,3,4,5,6,7,8,9,10,11]
 
         def check(author):
             def check2( answer ):
@@ -79,11 +79,11 @@ async def on_message( message ):
             return check2
             
         count = 0
-        while count < 3 :
+        while count < 6 :
 
             current = random.choice( questions )
-            if current == 1 :
-                await message.channel.send("Have you ever thought that if you dug in your backyard you could find buried treasure? (Send the number corresponding to your answer.)\n\n1) Yes.\n2) No.")          
+            if current == 0 :
+                await message.channel.send("Have you ever thought that if you dug in your backyard you could find buried treasure? \n\n1) Yes.\n2) No.")          
                 done = False
                 while done == False :
 
@@ -94,6 +94,23 @@ async def on_message( message ):
                     elif temp.content.find("2") != -1 :
                         personalities[2][1] += 1
                         done = True
+                    else :
+                        await temp.channel.send("Invalid Response, Try Again.")
+
+            elif current == 1 :
+                await message.channel.send("You're in class when you realize that you really have to go to the restroom! What do you do? \n\n1) Ask for permission to leave.\n2) Sneak out.\n3) Hold on until class ends.")          
+                done = False
+                while done == False :
+
+                    temp = await client.wait_for('message', check=check(message.author) )
+                    if temp.content.find("1") != -1 :
+                        personalities[0][1] += 2
+                        done = True
+                    elif temp.content.find("2") != -1 :
+                        personalities[4][1] += 1
+                        done = True
+                    elif temp.content.find("3") != -1 :
+                        personalities[2][1] += 1
                     else :
                         await temp.channel.send("Invalid Response, Try Again.")
 
@@ -135,6 +152,7 @@ async def on_message( message ):
                     temp = await client.wait_for('message', check=check(message.author) )
                     if temp.content.find("1") != -1 :
                         personalities[0][1] += 1
+                        personalities[3][1] += 1
                         done = True
                     elif temp.content.find("2") != -1 :
                         personalities[2][1] += 2
@@ -149,12 +167,139 @@ async def on_message( message ):
 
                     temp = await client.wait_for('message', check=check(message.author) )
                     if temp.content.find("1") != -1 :
-                        personalities[3][1] += 1
+                        personalities[3][1] += 2
+                        personalities[4][1] += 1
                         done = True
                     elif temp.content.find("2") != -1 :
                         personalities[2][1] += 1
-                        personalities[1][1] += 2
+                        personalities[0][1] += 1
                         done = True
+                    else :
+                        await temp.channel.send("Invalid Response, Try Again.")
+            
+            elif current == 6 :
+                await message.channel.send("Everyone around you is laughing hard at something you think is pretty boring. What do you do? \n\n1) Nothing.\n2) Laugh Along.\n3) It depends on the situation.")          
+                done = False
+                while done == False :
+
+                    temp = await client.wait_for('message', check=check(message.author) )
+                    if temp.content.find("1") != -1 :
+                        personalities[0][1] += 2
+                        done = True
+                    elif temp.content.find("2") != -1 :
+                        personalities[2][1] += 2
+                        done = True
+                    elif temp.content.find("3") != -1 :
+                        personalities[1][1] += 1
+                    else :
+                        await temp.channel.send("Invalid Response, Try Again.")
+
+            elif current == 7 :
+                await message.channel.send("Good news and bad news... Which one do you want to hear first? \n\n1) The good news.\n2) The bad news.\n3) I don't want either.")          
+                done = False
+                while done == False :
+
+                    temp = await client.wait_for('message', check=check(message.author) )
+                    if temp.content.find("1") != -1 :
+                        personalities[4][1] += 1
+                        done = True
+                    elif temp.content.find("2") != -1 :
+                        personalities[0][1] += 1
+                        personalities[3][1] += 2
+                        done = True
+                    elif temp.content.find("3") != -1 :
+                        personalities[2][1] += 1
+                    else :
+                        await temp.channel.send("Invalid Response, Try Again.")
+
+            elif current == 8 :
+                await message.channel.send("There's a rumor around about a ghost haunting the school bathrooms! What do you do?	 \n\n1) Avoid them.\n2) Go in there anyway.\n3) Bring your friends with you.")          
+                done = False
+                while done == False :
+
+                    temp = await client.wait_for('message', check=check(message.author) )
+                    if temp.content.find("1") != -1 :
+                        personalities[1][1] += 1
+                        personalities[0][1] -= 1
+                        done = True
+                    elif temp.content.find("2") != -1 :
+                        personalities[0][1] += 1
+                        personalities[4][1] += 1
+                        done = True
+                    elif temp.content.find("3") != -1 :
+                        personalities[2][1] += 1
+                    else :
+                        await temp.channel.send("Invalid Response, Try Again.")
+            
+            elif current == 9 :
+                await message.channel.send("You see a cake that is past its expiration date, but only by one day. What do you do? \n\n1) Not a problem, eat it anyway.\n2) Think about it briefly, then decide.\n3) Get someone to try it first.")          
+                done = False
+                while done == False :
+
+                    temp = await client.wait_for('message', check=check(message.author) )
+                    if temp.content.find("1") != -1 :
+                        personalities[4][1] += 2
+                        personalities[1][1] += 1
+                        done = True
+                    elif temp.content.find("2") != -1 :
+                        personalities[2][1] += 1
+                        done = True
+                    elif temp.content.find("3") != -1 :
+                        personalities[0][1] += 1
+                    else :
+                        await temp.channel.send("Invalid Response, Try Again.")
+
+            elif current == 10 :
+                await message.channel.send("You're eating at a very fancy restaurant known for its food. Which course do you select? \n\n1) Whatever is cheapest.\n2) Healthy fish.\n3) Anything, it's all good.")          
+                done = False
+                while done == False :
+
+                    temp = await client.wait_for('message', check=check(message.author) )
+                    if temp.content.find("1") != -1 :
+                        personalities[1][1] += 1
+                        done = True
+                    elif temp.content.find("2") != -1 :
+                        personalities[0][1] -= 1
+                        done = True
+                    elif temp.content.find("3") != -1 :
+                        personalities[2][1] += 1
+                        personalities[3][1] += 2
+                    else :
+                        await temp.channel.send("Invalid Response, Try Again.")
+
+            elif current == 11 :
+                await message.channel.send("You find something at a great bargain price. What do you do? \n\n1) Buy it right away.\n2) Think about whether you need it.\n3) Demand an even bigger discount.")          
+                done = False
+                while done == False :
+
+                    temp = await client.wait_for('message', check=check(message.author) )
+                    if temp.content.find("1") != -1 :
+                        personalities[1][1] += 1
+                        done = True
+                    elif temp.content.find("2") != -1 :
+                        personalities[0][1] -= 1
+                        done = True
+                    elif temp.content.find("3") != -1 :
+                        personalities[2][1] += 1
+                        personalities[3][1] += 2
+                    else :
+                        await temp.channel.send("Invalid Response, Try Again.")
+
+            elif current == 12 :
+                await message.channel.send("You find something at a great bargain price. What do you do? \n\n1) Buy it right away.\n2) Think about whether you need it.\n3) Demand an even bigger discount.")          
+                done = False
+                while done == False :
+
+                    temp = await client.wait_for('message', check=check(message.author) )
+                    if temp.content.find("1") != -1 :
+                        personalities[1][1] += 1
+                        done = True
+                    elif temp.content.find("2") != -1 :
+                        personalities[0][1] -= 1
+                        done = True
+                    elif temp.content.find("3") != -1 :
+                        personalities[2][1] += 1
+                        personalities[3][1] += 2
                     else :
                         await temp.channel.send("Invalid Response, Try Again.")
 
@@ -177,6 +322,7 @@ async def on_message( message ):
 
         #Send Nature to Discord Chat
         await message.channel.send(f"You are the {nature[0]} type.")
+
 
 @client.command
 async def ping(ctx):
