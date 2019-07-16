82e9192165e6879d331fbdb1f5d837495c33cddf
ericdkim78@gmail.com <ericdkim78@gmail.com>
2019-07-16 15:45:57 -0700

    Implemented play and stop, adjusted join

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 #Importing Discord 
 import discord
 from discord.ext import commands
+from discord.utils import get
 
 #Importing OsuAPI Python Wrapper
 import asyncio
@@ -12,6 +13,8 @@ from jikanpy import Jikan
 import json
 import pprint
 import random
+import youtube_dl
+import os
 #import PyNaCl
 
 #Config File Usage
@@ -21,7 +24,7 @@ with open( 'config.json') as config_file:
 apikey = config['osuapikey']
 
 #Instantiate API
-osu = OsuApi(apikey)
+osu_a = OsuApi(apikey)
 mal = Jikan()
 
 client = commands.Bot( command_prefix = '!')
@@ -40,12 +43,75 @@ allowed = ["gamingstats", "statsbot"]
 async def ping(ctx):
     await ctx.channel.send(f' {round(client.latency*1000)} MS')
 
-@client.command( pass_context = True )
+@client.command( pass_context = True, aliases= ['summon'])
 async def join(ctx):
-    user = ctx.message.author
-    voicechannel = discord.utils.get(ctx.guild.channels, name= 'The Fireplace' )
-    await voicechannel.connect()
+    global voice
+    currentchannel = ctx.message.author.voice.channel
+    voice = get( client.voice_clients, guild=ctx.guild )
 
+    if voice and voice.is_connected():
+        await voice.move_to( currentchannel )
+    else:
+        voice = await currentchannel.connect()
+        print(f'The bot has connected to {currentchannel}')
+    #await voice.disconnect()
+
+@client.command( pass_context = True, aliases = ['exit'])
+async def leave(ctx):
+    currentchannel = ctx.message.author.voice.channel
+    voice = get( client.voice_clients, guild=ctx.guild )
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        print("The bot has disconnected from voice chat")
+
+@client.command( pass_context = True, aliases = ['p'])
+async def play(ctx, url: str):
+    localsong = os.path.isfile("song.mp3")
+
+    try:
+        if localsong:
+            os.remove("song.mp3")
+    except PermissionError:
+        print("Tried to delete currently playing song")
+        return
+
+    voice = get(client.voice_clients, guild= ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL( ydl_opts ) as ydl:
+        ydl.download( [url] )
+
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            name = file
+            os.rename(file, "song.mp3")
+
+    voice.play( discord.FFmpegPCMAudio("song.mp3") )
+    voice.source = discord.PCMVolumeTransformer( voice.source )
+    voice.source.volume = 0.30
+
+@client.command( pass_context = True ) 
+async def osu(ctx, username: str):
+    user = await osu_a.get_user( username )
+    topscore = await osu_a.get_user_best( username )
+
+    if user != None :
+        beatmap = await osu_a.get_beatmap( beatmap_id = topscore.beatmap_id )
+        songname = beatmap.title
+        difficultyname = beatmap.version
+        stars = beatmap.difficultyrating
+
+        await ctx.send("IGN: " + username + "\nRank: #" + str(user.pp_rank) + "\nTop Play: " + songname + 
+            " [" + difficultyname + "]  " + str(round(stars,2)) + "*")
 
 #Text Commands
 @client.event
@@ -55,27 +121,12 @@ async def on_message( message ):
     channelcheck = False
     if str(message.channel) in allowed :
         channelcheck = True
-
-    if channelcheck and message.content.find("!osu") != -1 :
-        username = realmsg[5:(len(realmsg))]
-        user = await osu.get_user( username )
-        topscore = await osu.get_user_best( username )
-
-        if user != None :
-            beatmap = await osu.get_beatmap( beatmap_id = topscore.beatmap_id )
-            songname = beatmap.title
-            difficultyname = beatmap.version
-            stars = beatmap.difficultyrating
-
-            await message.channel.send("Username: " + username + "\nRank: " + str(user.pp_rank) + "\nTop Play: " + songname + 
-                " [" + difficultyname + "]  " + str(round(stars,2)) + "*")
-        
-    elif channelcheck and message.content.find("!mal") != -1 :
+     
+    if channelcheck and message.content.find("!mal") != -1 :
         inputname = realmsg[5:(len(realmsg))]
         user = mal.user( username = inputname, request = 'animelist')
             
         await message.channel.send("Username: " )
-
     elif channelcheck and message.content.find("!qqq") != -1 :
         personalities = [  ["BOLD", 0], ["QUIRKY", 0], ["TIMID", 0], ["NAIVE", 0], ["HASTY", 0] ]
         questions = [0,1,2,3,4,5,6,7,8,9,10,11,12]
